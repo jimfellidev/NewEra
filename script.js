4602d19902d9de92cd1f19fb877f88fe69f0bc2a
Jim Felli <jimfellidev@gmail.com>
2020-03-21 07:39:11 -0400

    hells yaw to the filter

--- script.js
+++ script.js
@@ -197,43 +197,75 @@ $(".filter__close").click(function () {
 
 
 
+
+
+
+
+
+$(function() {
+  var $gallery = $(".type");
+  $inputs = $gallery.find("input");
+  // $inputs.attr("checked", "checked");
+  $inputs.on("change", function() {
+    $inputs.each(function(i, cb) {
+      if (!cb.checked) $(".hats__wrap").find("." + this.value).css("display", "none");
+    });
+    $inputs.each(function(i, cb) {
+      if (cb.checked) $(".hats__wrap").find("." + this.value).css("display", "inline");
+    });
+  });
+});
+
+
+
+
+// ADJUST SHOW ALL BUTTONS
+
+
+
 // hat filter
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
+// Execute the function and show all columns
+
+// xx filterSelection("all") 
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("column");
+//   if (c == "all") c = "";
+
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
-  }
-}
+
+//   xx for (i = 0; i < x.length; i++) {
+//     removeClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+//   }
+// }
 
 // Show filtered elements
-function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+
+// xx function addClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
 
 // Hide elements that are not selected
-function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+
+// xx function removeClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
 
 // Add active class to the current button (highlight it)
 
